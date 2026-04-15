aa02966d49e1a79548b0a582d19a4265170e8300
Sebastian Hopgood <sebastianhopgood@gmail.com>
2026-04-15 15:38:49 -0400

    Update staged_geolocation.sql

--- scripts/02_silver/staged_geolocation.sql
+++ scripts/02_silver/staged_geolocation.sql
@@ -1,30 +1,30 @@
 /* Logic:
-    - Create a clean geolocation data table to help with data cleanliness, table modeling, joins, and analysis 
-    - Clean and standardize geolocation data for mapping
-    - Coordinates (Lat/Lng) cast to FLOAT64 for high precision in Power BI
-    - Standardize city and state strings for consistency with other tables
-    - REGEXP_REPLACE used to remove symbols while keeping accented letters and TRIM to remove unnecessary spacing
-    - DISTINCT used to remove redundant coordinate pings for the same area
+    - Data Refinement: Standardize raw geolocation data for spatial modeling and analysis.
+    - Precision Mapping: Cast coordinates to FLOAT64 to ensure high precision in Power BI visuals.
+    - String Normalization: Apply consistent casing (UPPER/INITCAP) and use REGEXP_REPLACE 
+      to remove noise symbols while preserving accented Brazilian characters.
+    - Optimization: Use DISTINCT to remove redundant coordinate entries and reduce table size.
+    - Geographic Validation: Filter out "garbage" coordinates outside valid global bounds.
 */
 
 CREATE OR REPLACE TABLE `olist-360-e-commerce.staged_data.staged_geolocation` AS
 
 SELECT
-  -- Selects only unique zip codes, zip codes are converted to INT64; note that leading zeros will be dropped
+  -- Standardize zip codes as INT64; note that leading zeros will be dropped
   DISTINCT SAFE_CAST(geolocation_zip_code_prefix AS INT64) AS geolocation_zip_code_prefix,
 
-  -- Coordinates must be FLOAT64 to keep decimal percision for maps
+  -- Maintain decimal precision for geographic mapping
   SAFE_CAST(geolocation_lat AS FLOAT64) AS geolocation_lat,
   SAFE_CAST(geolocation_lng AS FLOAT64) AS geolocation_lng,
 
-  -- Cities are converted to STRING, stripped of special charcters using REGEXP_REPLACE, stripped of unnecessary spacing, and formatted into tile casing (INTCAP)
+   -- Clean city names: Remove symbols, trim whitespace, and apply Title Case
   SAFE_CAST(INITCAP(REGEXP_REPLACE(TRIM(geolocation_city), r'[^\p{L}\s]', '')) AS STRING) AS geolocation_city,
 
-  -- States are converted to STRING, stripped of unnecessary spacing, and formatted as uppercase (e.g CA)
+  -- Standardize state codes as uppercase (e.g., SP)
   SAFE_CAST(UPPER(TRIM(geolocation_state)) AS STRING) AS geolocation_state
 FROM `olist-360-e-commerce.raw_data.raw_olist_geolocation`
 
--- Make data rows only show if the location is within the normal latitude and longitude range
+-- Filter for valid geographic coordinates
 WHERE
   (geolocation_lat BETWEEN -90 AND 90) AND
   (geolocation_lng BETWEEN -180 AND 180)
