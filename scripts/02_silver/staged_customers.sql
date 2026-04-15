b3aa73032efd6ca49778d39d219a775eaed070b8
Sebastian Hopgood <sebastianhopgood@gmail.com>
2026-04-15 15:41:36 -0400

    Refine comments and formatting in staged_customers.sql
    Updated comments to clarify data processing steps and standardized formatting.

--- scripts/02_silver/staged_customers.sql
+++ scripts/02_silver/staged_customers.sql
@@ -1,6 +1,6 @@
 /* 
 Logic Overview:
-  - Data Refinement: Standardize raw e-commerce data into a cleaned Silver-layer table.
+  - Data Refinement: Standardize raw customer data into a cleaned Silver-layer table.
   - Primary Keys: Standardize IDs (LOWER/TRIM) to ensure 100% join compatibility across tables.
   - String Normalization: Apply consistent casing (UPPER for states, INITCAP for cities) and 
     remove special character noise to improve dashboard readability.
@@ -12,19 +12,19 @@ Logic Overview:
 CREATE OR REPLACE TABLE `olist-360-e-commerce.staged_data.staged_customers` AS
 
 SELECT
-  -- IDs are are converted to STRING, stripped of unnecessary spacing, and forced-lowered to prevent case-sensitivity issues during joins
+  -- Standardized ID: Trimmed and lowered for join compatibility
   SAFE_CAST(LOWER(TRIM(customer_id)) AS STRING) AS customer_id,
 
-  -- Customer duplicate IDs are converted to STRING, stripped of unnecessary spacing, and forced-lowered to prevent case-sensitivity issues during joins
+ -- Unique Identifier: Used to link customers across different order sessions
   SAFE_CAST(LOWER(TRIM(customer_unique_id)) AS STRING) AS customer_unique_id,
 
-  -- Zip codes are converted to INT64; note that leading zeros will be dropped
+  -- Zip Codes: Converted to INT64 (Note: Leading zeros will be dropped)
   SAFE_CAST(customer_zip_code_prefix AS INT64) AS customer_zip_code_prefix,
 
-  -- Customer cities are converted to STRING, cleanned of any special characters, stripped of unnecessary spacing, and formatted as title casing (INITCAP)
+  -- City Normalization: Cleansed of symbols and formatted to Title Case
   SAFE_CAST(INITCAP(REGEXP_REPLACE(TRIM(customer_city), r'[^\p{L}\s]', '')) AS STRING) AS customer_city,
 
-  -- Customer states are converted to STRING, stripped of unnecessary spacing, and formatted as uppercase (e.g. NY)
+  -- State Formatting: Standardized to uppercase strings (e.g., SP)
   SAFE_CAST(UPPER(TRIM(customer_state)) AS STRING) AS customer_state
 FROM `olist-360-e-commerce.raw_data.raw_olist_customers`
 
