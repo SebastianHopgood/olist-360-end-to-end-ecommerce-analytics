8e6eb4e156fef6613e5c358993846835856bd509
Sebastian Hopgood <sebastianhopgood@gmail.com>
2026-04-15 17:45:39 -0400

    Update staged_order_payments.sql

--- scripts/02_silver/staged_order_payments.sql
+++ scripts/02_silver/staged_order_payments.sql
@@ -1,10 +1,11 @@
 /* 
 Logic Overview:
   - Financial Accuracy: Casts payment values to FLOAT64 and rounds to 2 decimals for precise revenue tracking
-  - String Normalization: Standardizes payment types by replacing underscores with spaces and applying
+  - String Normalization: Standardizes payment types by replacing underscores with spaces and applying 
     Title Case (INITCAP) for professional reporting
   - Join Compatibility: Trims and force-lowers Order IDs to ensure a 100% match with the staged_orders table
-  - Data Validation: Enforces business rules by filtering out negative payments, installments, or sequences
+  - Data Validation: Enforces business rules by filtering out negative payments and ensures 
+    every record has a valid ID
   - Deduplication: Uses DISTINCT to remove redundant transaction records and prevent inflated revenue metrics
 */
 
@@ -26,7 +27,14 @@ SELECT DISTINCT
   -- Revenue Metric: Rounded to 2 decimal places for financial consistency
   ROUND(SAFE_CAST(payment_value AS FLOAT64),2) AS payment_value
 FROM `olist-360-e-commerce.raw_data.raw_olist_order_payments`
+
 WHERE 
+  -- Ensure payment sequence and installments are logically non-negative
   payment_sequential >= 0 AND
   payment_installments >= 0 AND
-  payment_value >= 0.00;
+
+  -- Exclude records with negative payment values to maintain accounting integrity
+  payment_value >= 0.00 AND
+
+  -- Data Integrity: Ensures every payment record is linked to a valid order
+  order_id IS NOT NULL;
