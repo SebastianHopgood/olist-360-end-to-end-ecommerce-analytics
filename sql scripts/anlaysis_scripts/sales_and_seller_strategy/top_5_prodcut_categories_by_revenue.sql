eee8d3add35a08a398b19b251313ea9b8051252f
Sebastian Hopgood <sebastianhopgood@gmail.com>
2026-04-17 17:00:38 -0400

    Add files via upload

---
+++ sql scripts/anlaysis_scripts/sales_and_seller_strategy/top_5_prodcut_categories_by_revenue.sql
@@ -0,0 +1,14 @@
+-- Identify the top 5 product categories by total revenue to focus the analysis
+SELECT
+  dp.product_category_name,
+  
+  -- Sum of item price represents the core product revenue
+  SUM(ofact.price) AS total_product_revenue
+FROM `olist-360-e-commerce.analytical_data.orders_fact` AS ofact
+INNER JOIN `olist-360-e-commerce.analytical_data.dim_products` AS dp
+  ON ofact.product_id = dp.product_id
+GROUP BY dp.product_category_name
+
+-- Order by highest revenue and limit to the top 5 for dashboard focus
+ORDER BY total_product_revenue DESC
+LIMIT 5;
